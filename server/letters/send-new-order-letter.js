3f7d687f7062c5564f40f5e748d4657f3ce1db09
gen-a <pugachevsky@gmail.com>
2019-03-29 22:52:10 +0200

    Auth controller test

--- server/letters/send-new-order-letter.js
+++ server/letters/send-new-order-letter.js
@@ -1,3 +1,5 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const { mail } = require('../services/mail');
 
 exports.sendNewOrderLetter = (email, number, visaExpirationDate) => {
