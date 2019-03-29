70c200bb23d64036f1b7ab934a45774e9a5dc926
gen-a <pugachevsky@gmail.com>
2019-03-29 23:26:25 +0200

    Auth controller test

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -94,13 +94,23 @@ exports.email = (req, res, next) => {
         const password = generatePassword();
         const _id = new mongoose.Types.ObjectId();
         const user = new User({ _id, email, password });
-        return user.save()
+        return user.save((err)=>{
+          console.log(err);
+
+
+        });
+
+
+
+/*        return user.save()
           .then(() => sendRegistrationLetter(email, password))
           .then(() => {
             res.status(200).json(response({ email, isNew: true }, 'auth.info.passwordHasBeenSent', 0));
             return next();
           })
-          .catch(console.log);
+          .catch(console.log);*/
+
+
       } else {
         return Promise.resolve()
           .then(() => {
