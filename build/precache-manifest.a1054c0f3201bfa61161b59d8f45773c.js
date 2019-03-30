7a86edaac31860b3ca57e7a65fbfa45b0d393da8
gen-a <pugachevsky@gmail.com>
2019-03-30 21:07:47 +0200

    Build react app

---
+++ build/precache-manifest.a1054c0f3201bfa61161b59d8f45773c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "287907f48f037c530df4",
+    "url": "/static/js/5.287907f4.chunk.js"
+  },
+  {
+    "revision": "16c7f4a4136798499c6d",
+    "url": "/static/js/0.16c7f4a4.chunk.js"
+  },
+  {
+    "revision": "cab072fde5de2a79155b",
+    "url": "/static/js/main.cab072fd.chunk.js"
+  },
+  {
+    "revision": "9fbbab6c9c4f622af3d7",
+    "url": "/static/js/runtime~main.9fbbab6c.js"
+  },
+  {
+    "revision": "b8854668b836c9f5062f64cd55bfca0b",
+    "url": "/static/media/icon-speed.b8854668.svg"
+  },
+  {
+    "revision": "70d12cd4a08edd22a390",
+    "url": "/static/js/3.70d12cd4.chunk.js"
+  },
+  {
+    "revision": "72df0119f85b756712c4233e5265f875",
+    "url": "/static/media/icon-reliability.72df0119.svg"
+  },
+  {
+    "revision": "702c24fd2973a20c4093",
+    "url": "/static/js/4.702c24fd.chunk.js"
+  },
+  {
+    "revision": "143601fa7b7f1a344cd9604a10693b11",
+    "url": "/static/media/icon-online.143601fa.svg"
+  },
+  {
+    "revision": "88b6974d743d559b58000e95fb2bc396",
+    "url": "/static/media/mark-sale.88b6974d.svg"
+  },
+  {
+    "revision": "3b7e3d9b85e44415a2b4af7430e21da5",
+    "url": "/static/media/product-mark-popular.3b7e3d9b.svg"
+  },
+  {
+    "revision": "f63dfe8050b772fdfa04110ad82396f6",
+    "url": "/static/media/product-mark-new.f63dfe80.svg"
+  },
+  {
+    "revision": "cab072fde5de2a79155b",
+    "url": "/static/css/main.eb941965.chunk.css"
+  },
+  {
+    "revision": "702c24fd2973a20c4093",
+    "url": "/static/css/4.6fe1eb09.chunk.css"
+  },
+  {
+    "revision": "70d12cd4a08edd22a390",
+    "url": "/static/css/3.58a473e0.chunk.css"
+  },
+  {
+    "revision": "50b582e34225f92e100682e134417cbb",
+    "url": "/index.html"
+  }
+];
--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API_PREFIX = `${process.env.REACT_APP_SERVER_URL}/data`;
+export const API_PREFIX = `${process.env.REACT_APP_SERVER_URL || ''}/data`;
 export const URL_FETCH_USER = `${API_PREFIX}/fetch-user`;
 export const URL_FETCH_DICTIONARY_ARTICLES = `${API_PREFIX}/dictionary-articles`;
 
